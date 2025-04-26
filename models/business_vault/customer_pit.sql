b6b4794e15017641eafaefc384f15b60a0bad936
Alexander Chaykovskiy <fortochnikbiriya@gmail.com>
2025-04-26 08:46:57 +0000

    Обновление Data Vault моделей и конфигурации проекта

--- models/business_vault/customer_pit.sql
+++ models/business_vault/customer_pit.sql
@@ -2,23 +2,23 @@
 
 {%- set yaml_metadata -%}
 source_model: hub_customer
-src_pk: CUSTOMER_PK
-as_of_dates_table: AS_OF_DATE
+src_pk: customer_pk
+as_of_dates_table: as_of_date
 satellites:
-  SAT_CUSTOMER:
+  sat_customer:
     pk:
-      PK: CUSTOMER_PK
+      pk: customer_pk
     ldts:
-      LDTS: LOAD_DATE
-  SAT_CUSTOMER_CRM:
+      ldts: load_date
+  sat_customer_crm:
     pk:
-      PK: CUSTOMER_PK
+      pk: customer_pk
     ldts:
-      LDTS: LOAD_DATE
-stage_tables:
-  STG_CUSTOMERS: LOAD_DATE
-  STG_CUSTOMERS_CRM: LOAD_DATE
-src_ldts: LOAD_DATE
+      ldts: load_date
+src_ldts: load_date
+stage_tables_ldts:
+  stg_customers: load_date
+  stg_customers_crm: load_date
 {%- endset -%}
 
 {% set metadata_dict = fromyaml(yaml_metadata) %}
@@ -27,10 +27,11 @@ src_ldts: LOAD_DATE
 {% set src_pk = metadata_dict['src_pk'] %}
 {% set as_of_dates_table = metadata_dict['as_of_dates_table'] %}
 {% set satellites = metadata_dict['satellites'] %}
-{% set stage_tables_ldts = metadata_dict['stage_tables_ldts'] %}
 {% set src_ldts = metadata_dict['src_ldts'] %}
+{% set stage_tables_ldts = metadata_dict['stage_tables_ldts'] %}
 
-{{ automate_dv.pit(source_model=source_model, src_pk=src_pk,
+{{ automate_dv.pit(source_model=source_model, 
+                   src_pk=src_pk,
                    as_of_dates_table=as_of_dates_table,
                    satellites=satellites,
                    stage_tables_ldts=stage_tables_ldts,
